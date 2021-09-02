c4f4f2afeec89c7f19a5eebe5d566f4b002924f7
Tom Logan <tom.logan@canterbury.ac.nz>
2021-09-02 17:03:37 +1200

    code for computing the number of people, by demographic and county, exposed to SLR

---
+++ src/calc_exposed_people.py
@@ -0,0 +1,130 @@
+'''
+loop through each state
+- import the extent from 10ft slr+low
+- import the building footprint file
+- get all of the footprints that touch the extent 
+- save to an SQL db
+
+'''
+
+from slack import post_message_to_slack
+import main
+import geopandas as gpd
+import pandas as pd
+import yaml
+import numpy as np
+from tqdm import tqdm
+
+
+# config
+with open('./config/main.yaml') as file:
+    config = yaml.load(file)
+# functions - logging
+import logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
+
+# open connection to DB
+crs = config['set_up']['projection']
+db = main.init_db(config)
+con = db['con']
+engine = db['engine']
+conn = db['engine'].raw_connection()
+cur = conn.cursor()
+
+# demographic info
+demographics = ['U7B001', 'U7B003', 'U7B004', 'U7C002']
+
+
+# import code
+# code.interact(local=locals())
+
+region_set = [("ALFL_MOB_TLH", 'Alabama'),( "ALFL_MOB_TLH", 'Florida'),( "CA_ChannelI", 'California'),( "CA_EKA", 'California'),( "CA_LOX", 'California'),( "CA_MTR", 'California'),(
+               "CA_SGX", 'California'),( "CT", 'Connecticut'),( "DC", 'DistrictofColumbia'),( "DE", 'Delaware'),( "FL_JAX", 'Florida'),( "FL_MFL", 'Florida'),( "FL_MLB", 'Florida'),(
+               "FL_TBW", 'Florida'),( "GA", 'Georgia'),( "HI_Haw", 'Hawaii'),( "HI_Kau", 'Hawaii'),( "HI_L", 'Hawaii'),( "HI_Mau", 'Hawaii'),( "HI_Molok", 'Hawaii'),( "HI_Oahu", 'Hawaii'),(
+               "LA_CentralEast_slr_final_dist_poly", 'Louisiana'),( "LA_CentralNorth_slr_final_dist_poly", 'Louisiana'),( "LA_Central_slr_final_dist_poly", 'Louisiana'),(
+               "LA_Delta_slr_final_dist_poly", 'Louisiana'),( "LA_LakePontchartrain_slr_final_dist_poly", 'Louisiana'),( "LA_West_slr_final_dist_poly", 'Louisiana'),(
+               "MA",'Massachusetts'),("MD_E",'Maryland'),("MD_North",'Maryland'),("MD_Southe",'Maryland'),("MD_Southwe",'Maryland'),("MD_We",'Maryland'),("ME_E",'Maine'),("ME_We",'Maine'),(
+               "MS", 'Mississippi'),( "NC_Middle1", 'NorthCarolina'),( "NC_Middle2", 'NorthCarolina'),( "NC_Northe", 'NorthCarolina'),( "NC_Southern1", 'NorthCarolina'),(
+               "NC_Southern2", 'NorthCarolina'),( "NH", 'NewHampshire'),( "NJ_Middle", 'NewJersey'),( "NJ_Northe", 'NewJersey'),( "NJ_Southe", 'NewJersey'),( "NY_Hudso", 'NewYork'),(
+               "NY_Metro", 'NewYork'),( "NY_Suffolk", 'NewYork'),( "OR_MFR", 'Oregon'),( "OR_PQR", 'Oregon'),( "PA", 'Pennsylvania'),( "RI", 'RhodeIsland'),( "SC_Ce", 'SouthCarolina'),(
+               "SC_North", 'SouthCarolina'),( "SC_South", 'SouthCarolina'),( "TX_Ce", 'Texas'),( "TX_North1", 'Texas'),( "TX_North2", 'Texas'),( "TX_South1", 'Texas'),( "TX_South2", 'Texas'),(
+               "VA_EasternShore", 'Virginia'),( "VA_Middle", 'Virginia'),( "VA_Northe", 'Virginia'),( "VA_Southe", 'Virginia'),( "WA_PQR", 'Washington'),( "WA_PugetNW", 'Washington'),(
+               "WA_PugetSW", 'Washington'),( "WA_SEW", 'Washington')]
+
+
+for region, state in tqdm(region_set):
+
+    # import the blocks that intersect with the inundation
+    # blocks2 = gpd.read_file('/homedirs/projects/data/usa/tlgdb_2020_a_us_block.gdb',
+    #                           driver='FileGDB', layer=0, mask=inundation)
+    # blocks = blocks.to_crs(crs)                          
+    sql = """WITH slr AS (SELECT geometry as geom FROM slr_raw WHERE region='{region}' AND rise='{rise}')
+            SELECT geoid, geoid_county, geometry, "U7B001", "U7B003", "U7B004", "U7C002"
+            FROM blocks20, slr
+            WHERE ST_Intersects(blocks20.geometry, slr.geom);
+            """.format(rise=10, region=region)
+    blocks = gpd.read_postgis(sql, con=con, geom_col='geometry')
+    blocks.drop_duplicates(inplace=True)
+    logger.info('Blocks imported')
+
+    # import footprints in these blocks
+    footprint_blocks = gpd.read_file('./data/raw/footprint/{}.geojson.zip'.format(state), mask=blocks)
+    # sjoin the block geoid to the footprint
+    # footprint_blocks1 = gpd.sjoin(footprint_blocks, blocks, how='left')
+    footprint_blocks = gpd.sjoin(footprint_blocks, blocks, how='left', op='within')
+    footprint_blocks.drop_duplicates(inplace=True)
+    # calculate the area of footprint groupby block
+    footprint_blocks = footprint_blocks.to_crs(3395)
+    footprint_blocks['area'] = footprint_blocks.geometry.area
+    block_areas = footprint_blocks[['geoid', 'area']].groupby('geoid').sum()
+    logger.info('Footprints imported and area calculated')
+
+    # calculate the population/area for that block
+    blocks.set_index('geoid', inplace=True)
+    blocks = blocks.join(block_areas, how='left')
+    blocks.dropna(inplace=True)
+    for i in demographics:
+        blocks['{}m2'.format(i)] = blocks[i]/blocks['area']
+
+
+    # loop through the heights
+    results = []
+    for rise in tqdm(np.arange(0,11)):
+        # import inundation
+        sql = "SELECT geometry FROM slr_raw WHERE region='{region}' AND rise='{rise}'".format(rise=rise, region=region)
+        inundation = gpd.read_postgis(sql, con=con, geom_col='geometry')
+        inundation = inundation.to_crs(crs)
+
+        # intersect footprint_blocks with inundation
+        footprint_exposed = gpd.read_file('./data/raw/footprint/{}.geojson.zip'.format(state), mask=inundation)
+        footprint_exposed = footprint_exposed.to_crs(crs)
+        # join population/area for block onto the 
+        footprint_exposed = gpd.sjoin(footprint_exposed, blocks, how='left', op='within')
+        footprint_exposed = footprint_exposed.to_crs(3395)
+        footprint_exposed['area'] = footprint_exposed.geometry.area
+        # multiply with area to determine population 
+        for i in demographics:
+            footprint_exposed[i] = footprint_exposed['{}m2'.format(i)]*footprint_exposed['area']
+        # sum to get population in this county, region
+        population_exposed = footprint_exposed[['geoid_county'] + demographics].groupby('geoid_county').sum()
+        population_exposed.reset_index(inplace=True)
+        # append to results
+        population_exposed = population_exposed[['geoid_county','U7B001','U7B003','U7B004','U7C002']]
+        population_exposed['rise'] = rise
+        population_exposed['state'] = state
+        results.append(population_exposed)
+    
+    # write to sql
+    results = pd.concat(results)
+    results.to_sql('exposed_people', con=db['engine'], if_exists='append', index=False)
+    post_message_to_slack("Estimate for SLR exposed people in {}, {} is complete".format(region,state))
+
+post_message_to_slack("Estimate for SLR exposed people for the entire USA is complete")
+
+
+
+
