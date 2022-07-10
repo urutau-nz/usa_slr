f3b64ff603aa8c79935045eaa2877382dcdc9170
tommlogan <tom.logan@canterbury.ac.nz>
2022-07-10 21:32:50 +1200

    adding states

--- src/plot_map.py
+++ src/plot_map.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import geopandas as gpd
+import us
+state_map_abbr = us.states.mapping('fips', 'abbr')
+state_map_name = us.states.mapping('fips', 'name')
+
 
 iso = pd.read_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/isolation_county.csv', dtype={'geoid_county':str})
 inu = pd.read_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/inundation_county.csv', dtype={'geoid_county':str})
@@ -33,8 +37,12 @@ inu = pd.read_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/inunda
 df = iso.merge(inu, on=['geoid_county','rise'], how='left', suffixes = ('_iso','_inu'))
 df = df.fillna(value=0)
 df['ratio'] = df['U7B001_iso'] / df['U7B001_inu']
-df.rename(columns={'U7B001_iso':'isolated','U7B001_inu':'inundated','U7B001_percentage_iso':'isolated_percentage','U7B001_percentage_inu':'inundated_percentage'})
+df.rename(columns={'U7B001_iso':'isolated','U7B001_inu':'inundated','U7B001_percentage_iso':'isolated_percentage','U7B001_percentage_inu':'inundated_percentage'},inplace=True)
 df.reset_index(inplace=True)
+df['state_code'] = df['geoid_county'].str[:2]
+df['state_name'] = df['state_code'].copy()
+df.replace({'state_code': state_map_abbr,
+                  'state_name': state_map_name}, inplace=True)
 df = df[['geoid_county','rise','ratio', 'isolated','inundated','isolated_percentage','inundated_percentage','state_code','state_name']]
 df.to_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/dashboard_county.csv')
 
@@ -42,8 +50,12 @@ iso = pd.read_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/isolat
 inu = pd.read_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/inundation_tract.csv', dtype={'geoid_tract':str})
 df = iso.merge(inu, on=['geoid_tract','rise'], how='left', suffixes = ('_iso','_inu'))
 df = df.fillna(value=0)
-df.rename(columns={'U7B001_iso':'isolated','U7B001_inu':'inundated','U7B001_percentage_iso':'isolated_percentage','U7B001_percentage_inu':'inundated_percentage'})
+df.rename(columns={'U7B001_iso':'isolated','U7B001_inu':'inundated','U7B001_percentage_iso':'isolated_percentage','U7B001_percentage_inu':'inundated_percentage'},inplace=True)
 df.reset_index(inplace=True)
+df['state_code'] = df['geoid_tract'].str[:2]
+df['state_name'] = df['state_code'].copy()
+df.replace({'state_code': state_map_abbr,
+                  'state_name': state_map_name}, inplace=True)
 df = df[['geoid_tract','rise', 'isolated','inundated','isolated_percentage','inundated_percentage','state_code','state_name']]
 df.to_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/dashboard_tract.csv')
 
@@ -51,7 +63,7 @@ iso = pd.read_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/isolat
 inu = pd.read_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/inundation_state.csv', dtype={'state_code':str})
 df = iso.merge(inu, on=['state_code','rise'], how='left', suffixes = ('_iso','_inu'))
 df = df.fillna(value=0)
-df.rename(columns={'U7B001_iso':'isolated','U7B001_inu':'inundated','U7B001_percentage_iso':'isolated_percentage','U7B001_percentage_inu':'inundated_percentage'})
+df.rename(columns={'U7B001_iso':'isolated','U7B001_inu':'inundated','U7B001_percentage_iso':'isolated_percentage','U7B001_percentage_inu':'inundated_percentage','state_name_iso':'state_name'},inplace=True)
 df.reset_index(inplace=True)
 df = df[['rise', 'isolated','inundated','isolated_percentage','inundated_percentage','state_code','state_name']]
 df.to_csv('/home/tml/CivilSystems/projects/access_usa_slr/results/dashboard_state.csv')
